2bebb0865b23870e00924964df0a9726a27f95d9
kotmth <kotmth@gmail.com>
2017-06-20 04:38:59 +0630

    flyer create

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+
+	<link rel="stylesheet" href="/css/app.css">
+
+	<div class="container">
+
+		@yield('content')
+	
+	</div>
+</head>
+<body>
+	
+</body>
+</html>
